062c32f7a798426fe9556fae0407ea0752450ba7
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-11 04:31:27 +0600

    added footer component

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Blogs from '../Blogs/Blogs';
+import Contact from '../Contact/Contact';
 import Doctors from '../Doctors/Doctors';
 import FeatureService from '../FeatureService/FeatureService';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
-import MakeAppointment from '../MakeAppoinment/MakeAppointment';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 import Testimonials from '../Testimonials/Testimonials';
 
@@ -17,6 +19,8 @@ const Home = () => {
             <Testimonials></Testimonials>
             <Blogs></Blogs>
             <Doctors></Doctors>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
