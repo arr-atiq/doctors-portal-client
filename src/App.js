c42a6420f8ffd59d43d06ecf327f312806366f88
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-12 14:08:31 +0600

    added login component and set private route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -7,20 +7,28 @@ import {
 } from "react-router-dom";
 import Home from './Components/Home/Home/Home';
 import Appointment from './Components/Appointment/Appointment/Appointment';
+import Login from './Components/Login/Login/Login';
+import PrivateRoute from './Components/Login/PrivateRoute/PrivateRoute';
 
-
+export const UserContext = createContext();
 const App = () => {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <Router>
-      <Switch>
-        <Route exact path = "/">
-          <Home></Home>
-        </Route>
-        <Route path = "/appointment">
-          <Appointment></Appointment>
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <PrivateRoute path="/appointment">
+            <Appointment></Appointment>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider >
   );
 };
 
---
+++ src/Components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAt79V3Pe25bzhTScAyvbhk8BjQ9G56C10",
+    authDomain: "doctors-portal-auth-b9d92.firebaseapp.com",
+    projectId: "doctors-portal-auth-b9d92",
+    storageBucket: "doctors-portal-auth-b9d92.appspot.com",
+    messagingSenderId: "602452258755",
+    appId: "1:602452258755:web:07dc225b7dcd81f859d776"
+  };
+  export default firebaseConfig;
